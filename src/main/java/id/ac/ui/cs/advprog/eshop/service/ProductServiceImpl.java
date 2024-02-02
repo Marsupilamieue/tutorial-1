ddce41a1049a1f3de8973a14dd4111e05e657e50
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-02-02 15:27:03 +0700

    add missing method

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -12,4 +12,21 @@ import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService{
     
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product create(Product product){
+        productRepository.create(product);
+        return product;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        Iterator<Product> productIterator = productRepository.findAll();
+        List<Product> allProduct = new ArrayList<>();
+        productIterator.forEachRemaining(allProduct::add);
+        return allProduct;
+    }
+
 }
