ca96026a4773e361f2568fedf7de8491ba91f68a
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-03-06 21:03:01 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -18,9 +18,62 @@ public class Payment {
     String status;
 
     public Payment(String id, Order order, String method, Map<String, String> paymentData){
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = "WAITING_PAYMENT";
+
+        if (order == null) {
+            throw new IllegalArgumentException("Order must not be null");
+        }
+
+        if (paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data must not be null or empty");
+        }
+
+        if (!PaymentMethod.contains(method)) {
+            throw new IllegalArgumentException("Invalid payment method");
+        }
     }
 
     public Payment(String id, Order order, String method, Map<String, String> paymentData, String  status){
+        this(id,order,method,paymentData);
+
+        String[] statusList = {"WAITING_PAYMENT", "REJECTED", "SUCCESS", "FAILED"};
+        if(Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))){
+            throw new IllegalArgumentException();
+        }else{
+            this.status = status;
+        }
+    }
+
+    public void setStatus(String status){
+        String[] statusList = {"WAITING_PAYMENT", "REJECTED", "SUCCESS", "FAILED"};
+        if(Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))){
+            throw new IllegalArgumentException();
+        }else{
+            this.status = status;
+        }
+    }
+
+    public void processVoucherPayment(String voucherCode) {
+        if (voucherCode.length() != 16 || !voucherCode.startsWith("ESHOP") || !voucherCode.substring(5).matches("\\d{4}[A-Z]{3}\\d{4}")) {
+            this.status = "REJECTED";
+        }else{
+            this.status = "SUCCESS";
+        }
+    }
+
+    public void processCODPayment(Map<String, String> paymentData) {
+        String address = paymentData.get("address");
+        String deliveryFee = paymentData.get("deliveryFee");
+
+        if (address == null || address.isEmpty() || deliveryFee == null || deliveryFee.isEmpty()) {
+            this.status = "REJECTED";
+        }else {
+            this.status = "SUCCESS";
+        }
     }
 
 }
